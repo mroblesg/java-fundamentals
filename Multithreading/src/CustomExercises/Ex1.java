4ebb66252d5261cc52ef0dcef084be48fc9dc5f2
Miguel Robles G <112932122+mroblesg@users.noreply.github.com>
2022-10-20 17:30:35 +0200

    FInished multithreading practices

---
+++ Multithreading/src/CustomExercises/Ex1.java
@@ -0,0 +1,43 @@
+package CustomExercises;
+
+class Iterable implements Runnable {
+
+    int sum = 0;
+    Thread t;
+
+    public Iterable(String threadName) {
+        t = new Thread(this, threadName);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+            System.out.println(t.getName() + " actual value is :" + sum);
+        }
+
+        System.out.println(t.getName() + " Sum is: " + sum);
+
+    }
+}
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Iterable obj1 = new Iterable("Thread 1");
+        Iterable obj2 = new Iterable("Thread 2");
+        Iterable obj3 = new Iterable("Thread 3");
+
+        obj1.t.start();
+        obj2.t.start();
+        obj3.t.start();
+
+        try {
+            obj1.t.join();
+            obj2.t.join();
+            obj3.t.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main Thread interrupted");
+        }
+
+    }
+}
